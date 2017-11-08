33ded8b4b958199d58adf6cb3ad2bbeb05cad213
Yee Eric <yeejfe@hotmail.com>
2017-11-08 16:06:09 +0800

    Update vulnerable-plugin.js

--- www/cordova/plugins/cordova-plugin-vulnerable/www/vulnerable-plugin.js
+++ www/cordova/plugins/cordova-plugin-vulnerable/www/vulnerable-plugin.js
@@ -1,5 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.vulnerableMethod = function (arg0, success, error) {
-    exec(success, error, 'vulnerable-plugin', 'vulnerableMethod', [arg0]);
+module.exports = {
+    vulnerableMethod: function (arg0, success, error) {
+        exec(success, error, 'vulnerable-plugin', 'vulnerableMethod', [arg0]);
+    }
 };
