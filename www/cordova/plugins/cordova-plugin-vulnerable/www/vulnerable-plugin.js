88706445da13277b150c331ae800ad583617ff5b
bernardyip <bernard_yip40@hotmail.com>
2017-11-08 16:09:27 +0800

    Revert "Update vulnerable-plugin.js"
    This reverts commit 33ded8b4b958199d58adf6cb3ad2bbeb05cad213.

--- www/cordova/plugins/cordova-plugin-vulnerable/www/vulnerable-plugin.js
+++ www/cordova/plugins/cordova-plugin-vulnerable/www/vulnerable-plugin.js
@@ -1,7 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports = {
-    vulnerableMethod: function (arg0, success, error) {
-        exec(success, error, 'vulnerable-plugin', 'vulnerableMethod', [arg0]);
-    }
+exports.vulnerableMethod = function (arg0, success, error) {
+    exec(success, error, 'vulnerable-plugin', 'vulnerableMethod', [arg0]);
 };
